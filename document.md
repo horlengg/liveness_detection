6655684dfdeb6abe834eb615c697c7f0f22cb4e0
Leng <horleng123@gmail.com>
2025-12-16 09:50:48 +0700

    init source code

---
+++ document.md
@@ -0,0 +1,43 @@
+
+
+# Liveness Detection For Android
+
+### Facial Detection - Google ML Kit
+
+### Face Anti Spoofing
+https://github.com/minivision-ai/Silent-Face-Anti-Spoofing
+
+
+### Mask Detection for KYC
+https://github.com/Cindyalifia/tflite-face-mask-detection-android/
+
+
+## Compare to Mevii Liveness Detection
+- Platform Support
+- Versions
+- Face Anti Spoofing(Passive Check)
+- UI Customization
+- App Size
+- Performance
+
+
+<!-- Google ML Kit Face Detection -->
+<!-- Not support 32bit device -->
+
+<!-- GoogleML + FaceAntiSpoofing + MaskDetection + tensorflow ~ 28MB (for 64bit)-->
+<!-- + Flutter Engine ~ 33MB for 64bit device -->
+
+<!-- ===> Version -->
+<!-- Android 
+minSdkVersion: 21
+targetSdkVersion: 35
+compileSdkVersion: 35 -->
+
+<!-- iOS #
+Minimum iOS Deployment Target: 15.5
+Xcode 15.3.0 or newer
+Swift 5 -->
+
+<!-- Upload source code to internal repository -->
+<!-- Make Document -->
+<!-- Check with Bong Theara test verify fingerprint image -->
---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:sample_liveness_app/pages/face_anti_spoofing_page.dart';
+import 'package:sample_liveness_app/pages/liveness_check_page.dart';
+import 'package:sample_liveness_app/pages/mask_detection_page.dart';
+import 'package:sample_liveness_app/widgets/button.dart';
+
+void main() async {
+  runApp(const SampleLivenessApp());
+}
+
+class SampleLivenessApp extends StatelessWidget {
+  const SampleLivenessApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        Widget page;
+        
+        switch (settings.name) {
+          case '/':
+            page = const HomePage();
+            break;
+          case '/do_liveness':
+            page = const LivenessCheckPage();
+          case '/do_face_anti_spoofing':
+            page = const FaceAntiSpoofingPage();
+          case '/do_mask_detection':
+            page = const MaskDetectionPage();
+            break;
+          // case '/done':
+          //   page = const PageDone();
+          //   break;
+          default:
+            page = const HomePage();
+        }
+
+        return PageRouteBuilder(
+          settings: settings,
+          pageBuilder: (context, animation, secondaryAnimation) => page,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = Offset(1.0, 0.0); // slide from right
+            const end = Offset.zero;
+            const curve = Curves.easeInOut;
+
+            var tween = Tween(begin: begin, end: end).chain(
+              CurveTween(curve: curve),
+            );
+
+            return SlideTransition(
+              position: animation.drive(tween),
+              child: child,
+            );
+          },
+          transitionDuration: const Duration(milliseconds: 200),
+        );
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFC7D9E9),
+      appBar: AppBar(
+        title: Text("Sample Liveness Check",style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 20),
+
+              AppButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/do_liveness");
+                },
+                label: "Start",
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomPageTransitionBuilder extends PageTransitionsBuilder {
+  const CustomPageTransitionBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    // No animation for first route
+    if (route.settings.name == '/') return child;
+
+    // Example: slide from right + fade
+    final tween = Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: Offset.zero,
+    ).chain(CurveTween(curve: Curves.easeInOut));
+
+    return SlideTransition(
+      position: animation.drive(tween),
+      child: FadeTransition(
+        opacity: animation,
+        child: child,
+      ),
+    );
+  }
+}
